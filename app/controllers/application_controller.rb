dfdd943e14ea1bde1eaa1b192136381fa74c6332
Filippo Gangi Dino <filippo.gangidino@welaika.com>
2015-11-07 18:22:50 +0100

    fix syntax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
   helper_method :correct_user?
 
   private
+
     def current_user
       begin
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -23,13 +24,13 @@ class ApplicationController < ActionController::Base
     def correct_user?
       @user = User.find(params[:id])
       unless current_user == @user
-        redirect_to root_url, :alert => "Access denied."
+        redirect_to root_url, alert: "Access denied."
       end
     end
 
     def authenticate_user!
       if !current_user
-        redirect_to root_url, :alert => 'You need to sign in for access to this page.'
+        redirect_to root_url, alert: 'You need to sign in for access to this page.'
       end
     end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
 
   def create
     auth = request.env["omniauth.auth"]
-    user = User.where(:provider => auth['provider'],
-                      :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
+    user = User.where(provider: auth['provider'],
+                      uid: auth['uid'].to_s).first || User.create_with_omniauth(auth)
     reset_session
     user.token = auth['credentials']['token']
     user.save
@@ -16,10 +16,10 @@ class SessionsController < ApplicationController
 
   def destroy
     reset_session
-    redirect_to root_url, :notice => 'Signed out!'
+    redirect_to root_url, notice: 'Signed out!'
   end
 
   def failure
-    redirect_to root_url, :alert => "Authentication error: #{params[:message].humanize}"
+    redirect_to root_url, alert: "Authentication error: #{params[:message].humanize}"
   end
 end
