96096965adbe3b649f56056ecea49ff8e0fecbed
Fabrizio Monti <fabrizio.monti@welaika.com>
2015-11-08 11:56:07 +0100

    Add validations to matches

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,7 +2,8 @@ class Match < ActiveRecord::Base
   UPDATE_INTERVAL = Integer(ENV.fetch("MATCH_UPDATE_INTERVAL"))
 
   belongs_to :user
-  validates :user_id, :score, presence: true
+  validates :user_id, :score, :status, presence: true
+  validates :score, numericality: { only_integer: true }
 
   enum status: { pending: 0, started: 1, finished: 2 }
 end
