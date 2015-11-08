a8330dd911085dc892fa14caacb3b40d8486b9f7
Fabrizio Monti <fabrizio.monti@welaika.com>
2015-11-08 15:00:11 +0100

    Refactoring ping user controller

--- app/controllers/ping_users_controller.rb
+++ app/controllers/ping_users_controller.rb
@@ -1,12 +1,11 @@
 class PingUsersController < ApplicationController
+  before_action :authenticate_user!
+
   def create
-    if current_user
-      current_user.update(last_ping_at: Time.zone.now)
-      respond_to do |format|
-        format.json { render json: { currently_playing_count: User.currently_playing.count } }
-      end
-    else
-      head :unprocessable_entity
+    current_user.update(last_ping_at: Time.zone.now)
+
+    respond_to do |format|
+      format.json { render json: { currently_playing_count: User.currently_playing.count } }
     end
   end
 end
