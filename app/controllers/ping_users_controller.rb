527e935a75b6bef23afb86be51e17384ae82074e
Fabrizio Monti <fabrizio.monti@welaika.com>
2015-11-08 10:21:30 +0100

    small refactoring

--- app/controllers/ping_users_controller.rb
+++ app/controllers/ping_users_controller.rb
@@ -1,9 +1,7 @@
 class PingUsersController < ApplicationController
   def create
     if current_user
-      current_user.last_ping_at = Time.zone.now
-      current_user.save!
-
+      current_user.update(last_ping_at: Time.zone.now)
       respond_to do |format|
         format.json { render json: { currently_playing_count: User.currently_playing.count } }
       end
