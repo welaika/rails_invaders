1ca8db396f05f1631a8f7dbd14ed60abef62231b
Fabrizio Monti <fabrizio.monti@welaika.com>
2015-11-08 22:28:25 +0100

    fix copty

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < ActionController::Base
 
     def authenticate_user!
       if !current_user
-        redirect_to root_url, alert: 'You need to sign in for access to this page.'
+        redirect_to root_url, alert: 'You need to sign in to access this page.'
       end
     end
 end
